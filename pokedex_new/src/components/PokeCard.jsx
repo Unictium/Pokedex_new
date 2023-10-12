0a5276111ec14137730016e9b55ab1b55421283d
Mathieu <mathieuperrin77@gmail.com>
2023-10-12 11:15:57 +0200

    quete3

--- pokedex_new/src/components/PokeCard.jsx
+++ pokedex_new/src/components/PokeCard.jsx
@@ -1,7 +1,22 @@
+const pokemonList = [
+
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
 function PokemonCard () {
-    return (<figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"></img>
-        <figcaption>Bulbasaur</figcaption>
-        </figure>)
+    let pokemon = pokemonList[1];
+    return <figure>{ pokemon.imgSrc?
+        <img src={pokemon.imgSrc}></img> : <p>???</p>}
+        <figcaption>{pokemon.name}</figcaption>
+        </figure> 
 };
  export default PokemonCard;
+
+ 
